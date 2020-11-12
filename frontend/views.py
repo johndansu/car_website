91dae363d7a073e6422053f9236a3e8807dcf639
johndansu <dansujw@gmail.com>
2020-11-12 12:40:27 -0800

    added some changes

--- frontend/views.py
+++ frontend/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'frontend/index.html')
+
+def error(request):
+    return render(request, 'frontend/404error.html')
+
+def about(request):
+    return render(request, 'frontend/about-us.html')
+
+def car(request):
+    return render(request, 'frontend/car-detail.html')
